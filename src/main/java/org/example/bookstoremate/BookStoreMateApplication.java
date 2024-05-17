c66e0fe3cc904fa00438b6af23c07d2723de0a77
Shr1mpa <aminakrevetka@gmail.com>
2024-05-17 13:59:28 +0300

    initial commit

---
+++ src/main/java/org/example/bookstoremate/BookStoreMateApplication.java
@@ -0,0 +1,13 @@
+package org.example.bookstoremate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/bookstoremate/BookStoreMateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookstoremate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
