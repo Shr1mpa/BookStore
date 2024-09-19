5ceb09ccb82d275c3fbf9834334349b2a7b238dd
amina <aminakrevetka@gmail.com>
2024-09-19 12:41:04 +0300

    hw solution

--- src/main/java/org/example/bookstoremate/controller/BookController.java
+++ src/main/java/org/example/bookstoremate/controller/BookController.java
@@ -1,13 +1,17 @@
 package org.example.bookstoremate.controller;
 
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.bookstoremate.dto.BookDto;
 import org.example.bookstoremate.dto.CreateBookRequestDto;
 import org.example.bookstoremate.service.BookService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +35,25 @@ public class BookController {
     @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BookDto> updateBook(@PathVariable Long id, @RequestBody BookDto bookDto) {
+        try {
+            BookDto book = bookService.updateBook(id, bookDto);
+            return ResponseEntity.ok(book);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        try {
+            bookService.deleteBook(id);
+            return ResponseEntity.noContent().build();
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
